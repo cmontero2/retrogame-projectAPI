20cdb9b32d2fb1f6aed11c29eac795703e801ad1
cmontero2 <cmontero@iesfuentesanluis.org>
2021-02-10 13:12:40 +0100

    token funciona

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -10,15 +10,20 @@
         public function actionAuthenticate(){
             if ($_SERVER['REQUEST_METHOD'] === 'POST') {
               // Si se envían los datos en formato raw dentro de la petición http, se recogen así:
-              /*
-                $params=json_decode(file_get_contents("php://input"), false);
+              
+                
+              $params=json_decode(file_get_contents("php://input"), false);
               @$username=$params->username;
               @$password=$params->password;
-              */
+            
+              
               // Si se envían los datos de la forma habitual (form-data), se reciben en $_POST:
+              /*
               $username=$_POST['username'];
               $password=$_POST['password' ];
+              */
          
+              
               if($u=\app\models\Usuario::findOne(['user'=>$username]))
                   if($u->password==md5($password)) {//o crypt, según esté en la BD
          
