0aa127e9d398c74cd936561e881106c38d263d1a
cmontero2 <cmontero@iesfuentesanluis.org>
2021-02-16 13:20:17 +0100

    usuariosJuego filtro

--- controllers/UsuariosJuegoController.php
+++ controllers/UsuariosJuegoController.php
@@ -2,18 +2,29 @@
 namespace app\controllers;
 use yii\rest\ActiveController;
 use yii\filters\auth\HttpBearerAuth;
+use yii\data\ActiveDataProvider;
+use app\models\UsuarioJuego;
  
 class UsuariosJuegoController extends ApiController
 {
     public $modelClass = 'app\models\UsuarioJuego';
+    public $authenable=false;
 
-    public function behaviors() {
-        $behaviors = parent::behaviors();
-        $behaviors['authenticator'] = [
-           'class' => HttpBearerAuth::className(),
-           'except' => ['options', 'authenticate'],
-        ];
-        return $behaviors;
+    public function indexProvider() {
+        $usuario=$_GET['usuario']??"";
+        echo "hola ".$usuario;
+        if($usuario != ""){
+            return new ActiveDataProvider([
+                'query' => UsuarioJuego::find()
+                    ->where('usuario_id='.$usuario)
+                    ->orderBy('fecha_id')  
+            ]);
+        } else {
+            return new ActiveDataProvider([
+                'query' => UsuarioJuego::find()
+            ]);
+        }
     }
+    
 }
 ?>
